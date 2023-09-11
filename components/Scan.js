e3c209269e094c0e67c438f1c4892569fcdf4421
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-11 23:25:34 +0100

    scannner

--- components/Scan.js
+++ components/Scan.js
@@ -1,45 +1,73 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
+// import React, { Component } from 'react'
+// import QrReader from 'react-qr-scanner'
 
-class Scan extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            delay: 100,
-            result: 'No result',
-        }
+// class Scan extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             delay: 100,
+//             result: 'No result',
+//         }
 
-        this.handleScan = this.handleScan.bind(this)
-    }
-    handleScan(data) {
-        this.setState({
-            result: data,
-        })
-    }
-    handleError(err) {
-        console.error(err)
-    }
-    render() {
-        const previewStyle = {
-            height: 240,
-            width: 320,
-        }
+//         this.handleScan = this.handleScan.bind(this)
+//     }
+//     handleScan(data) {
+//         this.setState({
+//             result: data,
+//         })
+//     }
+//     handleError(err) {
+//         console.log("eeee")
+//         console.error(err)
+//     }
+//     render() {
+//         const previewStyle = {
+//             height: 240,
+//             width: 320,
+//         }
 
-        return (
-            <div>
-                <QrReader
-                    key="environment"
-                    constraints={{
-                        audio: false,
-                        video: { facingMode: "environment" }
-                    }}
-                    onError={this.handleError}
-                    onScan={this.handleScan}
-                />
-                <p>{this.state.result}</p>
-            </div>
-        )
-    }
-}
+//         return (
+//             <div>
+//                 <QrReader
+//                     key="environment"
+//                     constraints={{
+//                         audio: false,
+//                         video: { facingMode: "environment" }
+//                     }}
+//                     onError={this.handleError}
+//                     onScan={this.handleScan}
+//                 />
+//                 <p>{this.state.result}</p>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Scan
+
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+const Scan = (props) => {
+  const [data, setData] = useState('No result');
+
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
+      <p>{data}</p>
+    </>
+  );
+};
 
 export default Scan
--- pages/students/index.js
+++ pages/students/index.js
@@ -1,18 +1,33 @@
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import StudentHeader from '../../components/StudentHeader'
 import Image from 'next/image'
-import QrReader from 'react-qr-scanner'
+// import QrReader from 'react-qr-scanner'
 import AttendanceGrid from '../../components/AttendanceGrid'
 import Scan from '../../components/Scan'
+import { QrReader } from 'react-qr-reader';
 
 const Index = () => {
     const [data, setData] = useState('No result');
+    const [scanResultFile, setScanResultFile] = useState('');
     const [scan, setScan] = useState(false);
+    const qrRef = useRef(null)
 
 
     const scanCode = () => {
         setScan(true)
+    }
+
+    const onScanFile = () => {
+        qrRef.current.openImageDialog();
+    }
+    const handleErrorFile = (error) => {
+        console.log(error);
+    }
+    const handleScanFile = (result) => {
+        if (result) {
+            setScanResultFile(result);
+        }
     }
 
     return (
@@ -35,16 +50,21 @@ const Index = () => {
                                 <h1 className='text-[18px] md:text-[30px] medium text-[#141414]'>Class Attendance</h1>
                                 <div className='bg-[#183DA7] rounded-md py-[8px] px-[16px] md:flex space-x-[8px] cursor-pointer hidden' onClick={() => {
                                     setScan(true)
+                                    // onScanFile()
                                 }}>
                                     <Image src="/image/Frame.svg" width={20} height={20} alt="scan" />
                                     <h2 className='text-white'>Scan Code</h2>
                                 </div>
                                 <div className='fixed right-[20px] items-center justify-center flex bottom-[20%] bg-[#183DA7] rounded-full w-[48px] h-[48px] md:hidden' onClick={() => {
                                     setScan(true)
+                                    // onScanFile()
+
                                 }}>
                                     <Image src="/image/Frame.svg" width={20} height={20} alt="scan" />
                                 </div>
 
+
+                             
 
                             </div>
                             <AttendanceGrid />
