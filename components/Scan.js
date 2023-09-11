2ae55b53ee4aabd8a825a26f542e10ba98c7e4f6
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-11 23:32:03 +0100

    scan back

--- components/Scan.js
+++ components/Scan.js
@@ -49,25 +49,29 @@ import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 
 const Scan = (props) => {
-  const [data, setData] = useState('No result');
+    const [data, setData] = useState('No result');
 
-  return (
-    <>
-      <QrReader
-        onResult={(result, error) => {
-          if (!!result) {
-            setData(result?.text);
-          }
+    return (
+        <>
+            <QrReader
+                constraints={{
+                    audio: false,
+                    video: { facingMode: "environment" }
+                }}
+                onResult={(result, error) => {
+                    if (!!result) {
+                        setData(result?.text);
+                    }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        style={{ width: '100%' }}
-      />
-      <p>{data}</p>
-    </>
-  );
+                    if (!!error) {
+                        console.info(error);
+                    }
+                }}
+                style={{ width: '100%' }}
+            />
+            <p>{data}</p>
+        </>
+    );
 };
 
 export default Scan
