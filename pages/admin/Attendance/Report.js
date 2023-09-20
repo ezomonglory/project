d929bf58610b0e3cc938db13a92863ebfc3ceb40
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-20 04:38:25 +0100

    fixed error again

--- pages/admin/Attendance/Report.js
+++ pages/admin/Attendance/Report.js
@@ -1,16 +1,16 @@
-// import { useRouter } from 'next/navigation'
-// import React from 'react'
+import { useRouter } from 'next/navigation'
+import React from 'react'
 
-// const Report = () => {
-//     const router = useRouter()
-//     const query = router.query;
-//     const name = query.name;
-//     console.log(name)
-//     return (
-//         <div>
-//             hey
-//         </div>
-//     )
-// }
+const Report = () => {
+    // const router = useRouter()
+    // const query = router.query;
+    // const name = query.name;
+    // console.log(name)
+    return (
+        <div>
+            hey
+        </div>
+    )
+}
 
-// export default Report
+export default Report
