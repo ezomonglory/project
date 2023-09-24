9969952b316732695ed90b274c752f6d33caf46e
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-24 06:19:40 +0100

    ds

--- components/Attendance.js
+++ components/Attendance.js
@@ -102,7 +102,7 @@ const Attendance = ({ setLoad, load, selectedStudent, setSelectedCourse }) => {
                                         <div className='' onClick={() => {
                                             // router.push("/admin/Attendance/1")
                                         }}>
-                                            <div className='text-[12px] md:text-[12px] px-[12px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md cursor-pointer  inline-block' >
+                                            <div className='text-[14px] px-[18px] py-[8px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md cursor-pointer  inline-block' >
 
                                                 QR Code
                                             </div>
@@ -111,7 +111,7 @@ const Attendance = ({ setLoad, load, selectedStudent, setSelectedCourse }) => {
                                         <div className='' onClick={() => {
                                             router.push("/admin/Attendance/1")
                                         }}>
-                                            <div className='text-[12px] md:text-[12px] px-[12px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md cursor-pointer  inline-block' >
+                                            <div className='text-[14px] px-[18px] py-[8px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md cursor-pointer  inline-block' >
 
                                                 open
                                             </div>
@@ -160,14 +160,14 @@ const Attendance = ({ setLoad, load, selectedStudent, setSelectedCourse }) => {
                                 <div className='' onClick={() => {
                                     // router.push("/admin/Attendance/1")
                                 }}>
-                                    <div className='text-[12px] md:text-[12px] px-[12px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md text-center cursor-pointer  inline-block w-[80px] ' >
+                                    <div className='text-[14px] px-[12px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md text-center cursor-pointer  inline-block w-[100px] ' >
 
                                         QR Code
                                     </div>
                                 </div>
 
 
-                                <div className='text-[12px] md:text-[12px] px-[14px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md text-center cursor-pointer  inline-block w-[80px] '
+                                <div className='text-[14px] px-[14px] py-[6px] text-[#183DA7]  border-[2px] border-[#E2EAFE] medium rounded-md text-center cursor-pointer  inline-block w-[100px] '
                                     onClick={() => {
                                         router.push("/admin/Attendance/1")
                                     }}
