6b012a3b4f6676b950c89277a9a9393951aea43b
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-24 01:16:38 +0100

    image-renam

--- components/SidebarBtn.js
+++ components/SidebarBtn.js
@@ -3,7 +3,6 @@ import Image from "next/image"
 import { useRouter } from "next/router"
 
 
-
 const SidebarBtn = ({text, icon, href}) => {
     const router = useRouter()        
     const image = icon    
