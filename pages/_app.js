8b9e207b55b8d3425dec5594e7ae98c9973dc274
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-29 03:32:27 +0100

    fixed loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-[1400px] mx-auto ">
+        <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
