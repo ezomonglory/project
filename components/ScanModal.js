744fa3a1e1648d9e32d7c4d91c61a498be021837
Ezomon Glory <ezomonglory01@gmail.com>
2023-09-29 05:14:18 +0100

    scan

--- components/ScanModal.js
+++ components/ScanModal.js
@@ -30,16 +30,16 @@ const ScanModal = ({ qrCode }) => {
         setBtnLoad(true)
 
         await axios.post("https://attendx-2hi6.onrender.com/session/sign-qrCode", {
-            "qrCode":qrCode.text,
-            "full_name":user.full_name,
-            "matric_number":user.identity_number
-        }).then((res)=> {
-            console.log(res)                                                
+            "qrCode": qrCode.text,
+            "full_name": user.full_name,
+            "matric_number": user.identity_number
+        }).then((res) => {
+            console.log(res)
             setBtnLoad(false)
-            window.location.href="/students/"
-        }).catch((err)=> {
+            window.location.href = "/students/"
+        }).catch((err) => {
             console.log(err)
-        })                
+        })
     }
 
 
@@ -50,21 +50,23 @@ const ScanModal = ({ qrCode }) => {
     return (
 
         <div className='h-screen flex items-center justify-center bg-black p-[20px] '>
-            <div className='bg-white  rounded-[8px] md:w-[451px] md:h-[316px] flex flex-col space-y-[32px] py-[40px] px-[40px] mx-auto justify-center items-center'> 
+            <div className='bg-white  rounded-[8px] md:w-[451px] md:h-[316px] flex flex-col space-y-[32px] py-[40px] px-[40px] mx-auto justify-center items-center'>
 
-                <h1 className='text-[#8a8a8a] medium text-[16px] text-center ' >Submit Details</h1>
+                <div className='flex flex-col items-center space-y-[8px'>
+                    <h1 className='text-[#8a8a8a] medium text-[20px] leading-[24px] text-center ' >Student Details</h1>
 
-                <div className=' flex flex-col space-y-[8px] '>                   
-                   {user && (
-                    <div>                        
-                        <h1>Name: {user.full_name} </h1>
-                        <h1>Matric Number: {user.identity_number} </h1>                        
+                    <div className=' flex flex-col space-y-[8px] '>
+                        {user && (
+                            <div className='flex items-center flex-col' >
+                                <h1 className='uppercase' >Name: {user.full_name} </h1>
+                                <h1 className='uppercase' >Matric Number: {user.identity_number} </h1>
+                            </div>
+                        )}
                     </div>
-                   )}
                 </div>
 
                 <div className='flex  space-x-[32px]  '>
-                    <div className='w-[110px] h-[40px]   cursor-pointer bg-[#fff] text-[#323232]  flex items-center justify-center text-center rounded-[4px] medium leading-[24px] border-[0.5px] border-[#323232]  ' onClick={() => {
+                    <div className='w-[110px] h-[40px]   cursor-pointer bg-[#fff] text-[#323232]  flex items-center justify-center text-center rounded-[4px] medium leading-[24px] border-[1px] border-[#cdced9]  ' onClick={() => {
                         setOpen(false)
                     }}> Cancel </div>
 
